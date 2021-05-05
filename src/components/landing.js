ad79bf9f8b22eb5a054847ffc7e9ad69cfbc5b08
John Ratana <jratana1@gmail.com>
2021-05-04 23:44:24 -0400

    refactor

--- src/components/landing.js
+++ src/components/landing.js
@@ -36,9 +36,8 @@ const About = () => {
     return (
         <div className = "About">
             <p>
-                Chat-N-Draw is a drawing game.  Join the room and log-in to chat.  Choose a drawer.  The drawer submits an answer on the top right input, then start drawing.
-                Everyone in the room can see and guess the image in the chat.  First person to guess correctly wins (winner declared on the top right).
-                Click <Link to="/play">Here</Link> to play.
+                This is a front-end SPA built out for the Shopify Front-End internship.  
+                Click <Link to="/search">Here</Link> to try it out.
             </p>
         </div>
     )
